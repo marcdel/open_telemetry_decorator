0404fcbe9a334423f9547027cbf316f54e126174
Garth Kidd <garth@amplified.ai>
2020-08-08 11:18:00 +1000

    Configure project; port build and run-time dependencies

--- mix.exs
+++ mix.exs
@@ -1,28 +1,65 @@
 defmodule TelemetryDecorator.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
     [
       app: :telemetry_decorator,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      deps: deps(),
+      description: "A function decorator for telemetry",
+      docs: docs(),
+      elixir: "~> 1.8",
+      homepage_url: "https://github.com/amplifiedai/opentelemetry_decorator",
+      name: "TelemetryDecorator",
+      package: package(),
+      preferred_cli_env: [coveralls: :test, "coveralls.html": :test, "coveralls.json": :test],
+      source_url: "https://github.com/amplifiedai/opentelemetry_decorator",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      version: @version
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
+      {:decorator, "~> 1.3.2"},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13.0", only: :test, runtime: false},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false},
+      {:pretty, "~> 1.0.4"},
+      {:telemetry, "~> 0.4.2"}
+    ]
+  end
+
+  defp docs do
+    [
+      api_reference: false,
+      authors: ["Garth Kidd"],
+      canonical: "http://hexdocs.pm/telemetry_decorator",
+      main: "TelemetryDecorator",
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(mix.exs README.md lib),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "Amplified" => "https://www.amplified.ai",
+        "GitHub" => "https://github.com/amplifiedai/opentelemetry_decorator",
+        "OpenTelemetry Erlang SDK" => "https://github.com/open-telemetry/opentelemetry-erlang"
+      },
+      maintainers: ["Garth Kidd"]
     ]
   end
 end
