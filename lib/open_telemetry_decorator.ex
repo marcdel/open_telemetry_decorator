1e79f4b3a29ffb4a9e59169901a0d0e000cf98b9
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-09-12 22:06:02 -0700

    extract decorator and tests into new project

--- lib/open_telemetry_decorator.ex
+++ lib/open_telemetry_decorator.ex
@@ -3,16 +3,129 @@ defmodule OpenTelemetryDecorator do
   Documentation for `OpenTelemetryDecorator`.
   """
 
-  @doc """
-  Hello world.
+  use Decorator.Define, trace: 1, trace: 2
 
-  ## Examples
+  def trace(event_name, attr_keys \\ [], body, context) do
+    validate_args(event_name, attr_keys)
 
-      iex> OpenTelemetryDecorator.hello()
-      :world
+    event_name = Enum.join(event_name, ".")
 
-  """
-  def hello do
-    :world
+    quote location: :keep do
+      require OpenTelemetry.Span
+      require OpenTelemetry.Tracer
+
+      parent_ctx = OpenTelemetry.Tracer.current_span_ctx()
+
+      OpenTelemetry.Tracer.with_span unquote(event_name), %{parent: parent_ctx} do
+        result = unquote(body)
+
+        reportable_attrs =
+          OpenTelemetryDecorator.get_reportable_attrs(
+            Kernel.binding(),
+            unquote(attr_keys),
+            result
+          )
+
+        OpenTelemetry.Span.set_attributes(reportable_attrs)
+
+        result
+      end
+    end
+  rescue
+    e in ArgumentError ->
+      target = "#{inspect(context.module)}.#{context.name}/#{context.arity} @decorate telemetry"
+      reraise %ArgumentError{message: "#{target} #{e.message}"}, __STACKTRACE__
   end
+
+  def validate_args(event_name, attr_keys) do
+    if not (is_list(event_name) and atoms_only?(event_name) and not Enum.empty?(event_name)),
+      do: raise(ArgumentError, "event_name must be a non-empty list of atoms")
+
+    if Enum.empty?(event_name), do: raise(ArgumentError, "event_name is empty")
+
+    if not (is_list(attr_keys) and atoms_or_lists_of_atoms_only?(attr_keys)),
+      do:
+        raise(ArgumentError, "attr_keys must be a list of atoms, including nested lists of atoms")
+  end
+
+  def get_reportable_attrs(bound_variables, reportable_attr_keys, result \\ nil) do
+    bound_variables
+    |> take_attrs(reportable_attr_keys)
+    |> maybe_add_result(reportable_attr_keys, result)
+    |> remove_underscores()
+    |> stringify_list()
+    |> Enum.into([])
+  end
+
+  defp take_attrs(bound_variables, attr_keys) do
+    {keys, nested_keys} = Enum.split_with(attr_keys, &is_atom/1)
+
+    attrs = Keyword.take(bound_variables, keys)
+
+    nested_attrs =
+      nested_keys
+      |> Enum.map(fn key_list ->
+        key = key_list |> Enum.join("_") |> String.to_atom()
+        {obj_key, other_keys} = List.pop_at(key_list, 0)
+
+        with {:ok, obj} <- Keyword.fetch(bound_variables, obj_key),
+             {:ok, value} <- take_nested_attr(obj, other_keys) do
+          {key, value}
+        else
+          _ -> nil
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+
+    Keyword.merge(nested_attrs, attrs)
+  end
+
+  defp take_nested_attr(obj, keys) do
+    case get_in(obj, Enum.map(keys, &Access.key(&1, nil))) do
+      nil -> {:error, :not_found}
+      value -> {:ok, value}
+    end
+  end
+
+  defp maybe_add_result(attrs, attr_keys, result) do
+    if Enum.member?(attr_keys, :result) do
+      Keyword.put_new(attrs, :result, result)
+    else
+      attrs
+    end
+  end
+
+  defp remove_underscores(attrs) do
+    Enum.map(attrs, fn {key, value} ->
+      key =
+        key
+        |> Atom.to_string()
+        |> String.trim_leading("_")
+        |> String.to_existing_atom()
+
+      {key, value}
+    end)
+  end
+
+  defp stringify_list(attrs) do
+    Enum.map(attrs, fn {key, value} -> {key, stringify(value)} end)
+  end
+
+  defp stringify(thing) when is_map(thing) or is_struct(thing) or is_list(thing) do
+    inspect(thing)
+  end
+
+  defp stringify(thing), do: thing
+
+  defp atoms_or_lists_of_atoms_only?(list) when is_list(list) do
+    Enum.all?(list, fn item ->
+      (is_list(item) && atoms_or_lists_of_atoms_only?(item)) or is_atom(item)
+    end)
+  end
+
+  defp atoms_or_lists_of_atoms_only?(item) when is_atom(item) do
+    true
+  end
+
+  defp atoms_only?(list), do: Enum.all?(list, &is_atom/1)
 end
--- test/open_telemetry_decorator_test.exs
+++ test/open_telemetry_decorator_test.exs
@@ -1,8 +1,178 @@
 defmodule OpenTelemetryDecoratorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest OpenTelemetryDecorator
 
-  test "greets the world" do
-    assert OpenTelemetryDecorator.hello() == :world
+  describe "validate_args" do
+    test "event name must be a non-empty list of atoms" do
+      OpenTelemetryDecorator.validate_args([:name, :space, :event], [])
+
+      assert_raise ArgumentError, ~r/^event_name/, fn ->
+        OpenTelemetryDecorator.validate_args("name.space.event", [])
+      end
+    end
+
+    test "attr_keys can be empty" do
+      OpenTelemetryDecorator.validate_args([:name, :space, :event], [])
+    end
+
+    test "attrs_keys must be atoms" do
+      OpenTelemetryDecorator.validate_args([:name, :space, :event], [:variable])
+
+      assert_raise ArgumentError, ~r/^attr_keys/, fn ->
+        OpenTelemetryDecorator.validate_args([:name, :space, :event], ["variable"])
+      end
+    end
+
+    test "attrs_keys can contain nested lists of atoms" do
+      OpenTelemetryDecorator.validate_args([:name, :space, :event], [:variable, [:obj, :key]])
+    end
+  end
+
+  describe "get_reportable_attrs" do
+    test "all together" do
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs(
+          [count: 10, _name: "user name", obj: %{id: 1}],
+          [:count, :_name, [:obj, :id], :result],
+          {:ok, :success}
+        )
+
+      assert attrs == [result: {:ok, :success}, obj_id: 1, count: 10, name: "user name"]
+    end
+  end
+
+  describe "take_attrs" do
+    test "handles flat attributes" do
+      assert OpenTelemetryDecorator.get_reportable_attrs([id: 1], [:id]) == [id: 1]
+    end
+
+    test "handles nested attributes" do
+      assert OpenTelemetryDecorator.get_reportable_attrs([obj: %{id: 1}], [[:obj, :id]]) == [
+               obj_id: 1
+             ]
+    end
+
+    test "handles flat and nested attributes" do
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs([error: "whoops", obj: %{id: 1}], [
+          :error,
+          [:obj, :id]
+        ])
+
+      assert attrs == [obj_id: 1, error: "whoops"]
+    end
+
+    test "can take the top level element and a nested attribute" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([obj: %{id: 1}], [:obj, [:obj, :id]])
+      assert attrs == [obj_id: 1, obj: "%{id: 1}"]
+    end
+
+    test "handles multiply nested attributes" do
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs([obj: %{user: %{id: 2}}], [[:obj, :user, :id]])
+
+      assert attrs == [obj_user_id: 2]
+
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs(
+          [obj: %{user: %{track: %{id: 3}}}],
+          [[:obj, :user, :track, :id]]
+        )
+
+      assert attrs == [obj_user_track_id: 3]
+    end
+
+    test "does not add attribute if missing" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([obj: %{}], [[:obj, :id]])
+      assert attrs == []
+
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([], [[:obj, :id]])
+      assert attrs == []
+    end
+  end
+
+  describe "maybe_add_result" do
+    test "when :result is given, adds result to the list" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([], [:result], {:ok, "include me"})
+      assert attrs == [result: {:ok, "include me"}]
+
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs([id: 10], [:result, :id], {:ok, "include me"})
+
+      assert attrs == [result: {:ok, "include me"}, id: 10]
+    end
+
+    test "when :result is missing, does not add result to the list" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([], [], {:ok, "include me"})
+      assert attrs == []
+
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs([name: "blah"], [:name], {:ok, "include me"})
+
+      assert attrs == [name: "blah"]
+    end
+  end
+
+  describe "remove_underscores" do
+    test "removes underscores from keys" do
+      assert OpenTelemetryDecorator.get_reportable_attrs([_id: 1], [:_id]) == [id: 1]
+
+      assert OpenTelemetryDecorator.get_reportable_attrs([_id: 1, _name: "asd"], [:_id, :_name]) ==
+               [
+                 id: 1,
+                 name: "asd"
+               ]
+    end
+
+    test "doesn't modify keys without underscores" do
+      assert OpenTelemetryDecorator.get_reportable_attrs([_id: 1, name: "asd"], [:_id, :name]) ==
+               [
+                 id: 1,
+                 name: "asd"
+               ]
+    end
+  end
+
+  describe "stringify_list" do
+    test "doesn't modify strings" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([string_attr: "hello"], [:string_attr])
+      assert attrs == [string_attr: "hello"]
+    end
+
+    test "doesn't modify integers" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([int_attr: 12], [:int_attr])
+      assert attrs == [int_attr: 12]
+    end
+
+    test "stringifies maps" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([obj: %{id: 10}], [:obj])
+      assert attrs == [obj: "%{id: 10}"]
+    end
+
+    defmodule TestStruct do
+      defstruct [:id, :name]
+    end
+
+    test "stringifies structs" do
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs([obj: %TestStruct{id: 10, name: "User1"}], [
+          :obj
+        ])
+
+      assert attrs == [obj: "%OpenTelemetryDecoratorTest.TestStruct{id: 10, name: \"User1\"}"]
+    end
+
+    test "stringifies lists" do
+      attrs = OpenTelemetryDecorator.get_reportable_attrs([matches: [1, 2, 3, 4]], [:matches])
+      assert attrs == [matches: "[1, 2, 3, 4]"]
+
+      attrs =
+        OpenTelemetryDecorator.get_reportable_attrs(
+          [matches: [{"user 1", "user 2"}, {"user 3", "user 4"}]],
+          [:matches]
+        )
+
+      assert attrs == [matches: "[{\"user 1\", \"user 2\"}, {\"user 3\", \"user 4\"}]"]
+    end
   end
 end
