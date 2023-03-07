7ebe62a553d7fbf8e5b7bc972c9540733555d909
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-03-06 16:48:42 -0800

    bump dep versions

--- mix.exs
+++ mix.exs
@@ -31,14 +31,14 @@ defmodule OpenTelemetryDecorator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5.1", only: [:dev, :test], runtime: false},
-      {:decorator, "~> 1.4.0"},
-      {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.25.3", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
-      {:opentelemetry, "~> 1.0.0-rc.3", only: :test},
-      {:opentelemetry_api, "~> 1.0.0-rc.3"},
-      {:opentelemetry_exporter, "~> 1.0.0-rc.3", only: :test}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:decorator, "~> 1.4"},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.29.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.16.0", only: :test, runtime: false},
+      {:opentelemetry, "~> 1.2"},
+      {:opentelemetry_api, "~> 1.2"},
+      {:opentelemetry_exporter, "~> 1.4", only: :test}
     ]
   end
 
