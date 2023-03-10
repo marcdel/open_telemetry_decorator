14a4d2f9fb41ffe3fce62bd991f956f3de0e7409
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-03-10 14:50:26 -0800

    new config style

--- config/config.exs
+++ config/config.exs
@@ -1,34 +1,12 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# third-party users, it should be done in your "mix.exs" file.
+# General application configuration
+import Config
 
-# You can configure your application as:
-#
-#     config :opentelemetry_ecto, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:opentelemetry_ecto, :key)
-#
-# You can also configure a third-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-try do
-  import_config "#{Mix.env()}.exs"
-rescue
-  _ -> :ok
-end
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
