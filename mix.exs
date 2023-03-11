c0020b046c995448d4b5f62695db39bc47dac1b9
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-03-11 12:52:25 -0800

    bump library version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OpenTelemetryDecorator.MixProject do
   use Mix.Project
 
-  @version "1.2.2"
+  @version "1.2.3"
   @github_page "https://github.com/marcdel/open_telemetry_decorator"
 
   def project do
